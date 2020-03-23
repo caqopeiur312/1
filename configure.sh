953f8c0f13001a19eb93e0139d0ad02e230ec97c
li_borui <liborui0609@foxmail.com>
2020-03-23 17:26:28 +0800

    Update Everything

---
+++ configure.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# New Config
+cat > /etc/v2ray/config.json << EOF
+{
+    "inbounds": [
+        {
+            "port": $V2RAY_PORT,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$BACKEND_UUID",
+                        "alterId": 64
+                    }
+                ]
+            }
+        },
+        {
+            "port": 8188,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$FRONTEND_UUID",
+                        "alterId": 64
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "ws",
+                "wsSettings": {
+                    "path": "/$V2RAY_PATH"
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "settings": {}
+        }
+    ]
+}
+EOF
+
+# Run V2Ray
+/usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
